d8ed8c3627df7fd545553e15074530e804a8f5d2
althaf2 <althafnazeernazeer@gmail.com>
2023-09-20 19:56:03 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React,{useState,useEffect,useRef} from 'react';
 import './App.css';
 
 function App() {
+  const age = useRef(18);
+  const [x,setx] = useState(0);
+
+  function updateAge(){
+     age.current = age.current+1;
+     console.log(age.current);
+  }
+    
+
+  
+  const [name,setName] = useState("");
+  const [agee,setAge] = useState(0);
+  const [isAdult,setisAdult] = useState(false);
+
+  useEffect(()=>{
+    console.log("welcome");
+  },[])
+  useEffect(()=>{
+     setisAdult(age>=18)
+  },[age])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+
+      <button onClick={()=>setx(x+1)}>Re-render</button>
+      <button onClick={updateAge}>Update-age</button>
+      <button onClick={()=>console.log(age.current)}>Console log</button>
+{/* */}
+      
     </div>
   );
 }
